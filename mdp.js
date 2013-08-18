9157016565ecb64b90afa58210cff9db6c929720
Ravi Khadiwala <ravi.khadiwala@gmail.com>
2013-08-18 08:47:07 -0500

    wip

--- mdp.js
+++ mdp.js
@@ -1,14 +1,18 @@
 module markov
 
-export MDP, randommdp
+export MDP, randommdp, valueiteration
 
 type MDP
   P
   R
+  na
+  ns
   gamma
 end
 
-function randommdp(a,s)
+MDP(P,R,gamma) = MDP(P,R,size(P,1),size(P,2),gamma)
+
+function random_mdp(a,s)
     P = rand(a,s,s)
     R = rand(a,s,s)
     for i in 1:a
@@ -20,6 +24,26 @@ function randommdp(a,s)
     MDP(P,R,rand()) 
 end
 
-function valueiteration(mdp::MDP)
+function value_iteration(mdp::MDP, err)
+    V = rand(mdp.ns)
+    profit(s1,s2,a) = mdp.P[a,s1,s2] * (mdp.R[a,s1,s2] + mdp.gamma * V[s2])
+    error = err + 1
+    count = 0
+    while error > err
+      count += 1
+      error = 0.0
+      for s in 1:mdp.ns
+        last = V[s]
+        V[s] = max(map(a-> sum(map(s2 -> profit(s,s2,a), 1:mdp.ns)), 1:mdp.na))
+        error = abs(last - V[s]) > error ? abs(last - V[s]) : error
+      end
+    end
+    policy = zeros(mdp.ns)
+    for s in 1:mdp.ns
+        policy[s] = indmax(map(a->sum(map(s2->profit(s,s2,a), 1:mdp.ns)), 1:mdp.na))
+        policy[s] = indmax(sum(profit(s,s2,a) for s2 in 1:mdp.ns) for a in 1:mdp.na)
+    println(count)
+    return V
+end
   
 end
