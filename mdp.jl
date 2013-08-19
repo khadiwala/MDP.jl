0714b1248fae5d1cfbb720a0bed9ab4e8875155d
Ravi Khadiwala <ravi.khadiwala@gmail.com>
2013-08-19 01:39:42 -0500

    bugfixes

--- mdp.jl
+++ mdp.jl
@@ -1,6 +1,6 @@
 module markov
 
-export MDP, randommdp, valueiteration
+export MDP, random_mdp, value_iteration
 
 type MDP
   P
@@ -28,18 +28,17 @@ function value_iteration(mdp::MDP, err)
     V = rand(mdp.ns)
     profit(s1,s2,a) = mdp.P[a,s1,s2] * (mdp.R[a,s1,s2] + mdp.gamma * V[s2])
     error = err + 1
-    count = 0
     while error > err
-      count += 1
       error = 0.0
       for s=1:mdp.ns
         last = V[s]
-        V[s] = max(sum(profit(s,s2,a) for s2 in 1:mdp.ns) for a in 1:mdp.na)
-        error = abs(last - V[s]) > error ? abs(last - V[s]) : error
+        V[s] = max([sum([profit(s,s2,a) for s2 in 1:mdp.ns]) for a in 1:mdp.na])
+        delta = abs(last - V[s])
+        error = delta > error ? delta : error
       end
     end
-    policy = [indmax(sum(profit(s,s2,a) for s2 in 1:mdp.ns) for a in 1:mdp.na) for s in 1:mdp.ns]
-    return V,policy
+    policy = [indmax([sum([profit(s,s2,a) for s2 in 1:mdp.ns]) for a in 1:mdp.na]) for s in 1:mdp.ns]
+    return policy,V
 end
   
 end
