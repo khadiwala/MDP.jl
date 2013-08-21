c54918003fc1c96820821b33b79716f758b96618
Ravi Khadiwala <ravi.khadiwala@gmail.com>
2013-08-21 00:42:30 -0500

    imaps

--- mdp.jl
+++ mdp.jl
@@ -15,8 +15,8 @@ MDP(P,R,gamma) = MDP(P,R,size(P,1),size(P,2),gamma)
 function random_mdp(a,s)
     P = rand(a,s,s)
     R = rand(a,s,s)
-    for i in 1:a
-      for j in 1:s
+    for i=1:a
+      for j=1:s
         P[i,j,:] = P[i,j,:] / sum(P[i,j,:])
         R[i,j,:] = R[i,j,:] / sum(R[i,j,:])
       end
@@ -24,25 +24,32 @@ function random_mdp(a,s)
     MDP(P,R,rand()) 
 end
 
+function imap(f,itr)
+    @task for i=itr
+        produce(f(i))
+    end
+end
+
 function value_iteration(mdp::MDP, err)
     V = rand(mdp.ns)
-    profit(s1,s2,a) = mdp.P[a,s1,s2] * (mdp.R[a,s1,s2] + mdp.gamma * V[s2])
+
+    profits(s,a) = 
+    @task for sn=1:mdp.ns
+        produce(mdp.P[a,s,sn] * (mdp.R[a,s,sn] + mdp.gamma * V[sn]))
+    end
+
     maxerr = err + 1
     while maxerr > err
       maxerr = 0.0
       for s=1:mdp.ns
-        last = V[s]
-        V[s] = max([sum([profit(s,s2,a) for s2 in 1:mdp.ns]) for a in 1:mdp.na])
-        delta = abs(last - V[s])
+        prev = V[s]
+        V[s] = max(imap(sum, imap(a->profits(s,a), 1:mdp.na)))
+        delta = abs(prev - V[s])
         maxerr = delta > maxerr ? delta : maxerr
       end
     end
-    policy = [indmax([sum([profit(s,s2,a) for s2 in 1:mdp.ns]) for a in 1:mdp.na]) for s in 1:mdp.ns]
+    policy = [indmax([sum(profits(s,a)) for a=1:mdp.na]) for s=1:mdp.ns]
     return policy,V
 end
-
-function policy_iteration(mdp::MDP)
-    policy = rand(1:mdp.ns)
-end     
   
 end
